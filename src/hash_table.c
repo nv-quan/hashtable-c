34504c77837dbe082ff54774a78f3cc8184a1bbe
Quan Nguyen <quannguyenvan1701@gmail.com>
2019-03-01 21:07:26 +0900

    Update

--- src/hash_table.c
+++ src/hash_table.c
@@ -4,47 +4,67 @@
 #include "hash_table.h"
 #include <time.h>
 
-static pair* pair_init(const char* k, const char* v) {
-  pair* new_pair = (pair*) malloc(sizeof(pair));
+static str_pair* pair_init(const char* k, const char* v) {
+  str_pair* new_pair = (str_pair*) malloc(sizeof(str_pair));
   new_pair->key = strdup(k);
   new_pair->value = strdup(v);
   return new_pair;
 }
 
-static void pair_free(pair* p) {
+static void pair_free(str_pair* p) {
   free(p->key);
   free(p->value);
   free(p);
 }
 
-static table* table_init() {
-  table* new_table = (table*) malloc(sizeof(table));
+static hash_table* table_init() {
+  hash_table* new_table = (hash_table*) malloc(sizeof(hash_table));
   new_table->count = 0;
   new_table->size = TABLESIZE;
-  new_table->items = (pair**) malloc(sizeof(pair*) * TABLESIZE);
+  new_table->items = (str_pair**) calloc(TABLESIZE, sizeof(str_pair*));
   return new_table;
 }
 
-static void table_free(table* t) {
-  for (int i = 0; i < t->count; ++i) 
-    free(t->items[i]);
+static void table_free(hash_table* t) {
+  for (int i = 0; i < t->count; ++i)
+    if (t->items[i] != 0)
+      free(t->items[i]);
   free(t->items);
   free(t);
 }
 
-static int hash_code(const char* str, int a) {
-//a = 33 gives at most 6 collisions on a set of 50,000 English words
+static int hash_code(const char* str, int polynomial_const) {
   int code = 0;
   for (int i = strlen(str) - 1; i >= 0; --i)
-    code = code * a + str[i];
+    code = code * polynomial_const + str[i];
   return code;
 }
 
 static int compress_function(int hashcode, int a, int b, int N) {
-  return abs(a * hashcode + b) % N;
+  return (a * hashcode + b) % N;
 }
 
 static int second_function(int hashcode, int q) {
   //q must be prime
   return q - hashcode % q;
+}
+
+void init_constant(){
+  srand(time(NULL));
+  while (1) {
+    ht_const1 = rand();
+    ht_const2 = rand();
+    if (ht_const1 > 0 && ht_const1 % TABLESIZE != 0 && ht_const2 >= 0)
+      break;
+  }
+}
+  
+void ht_insert(hash_table* ht, const char* key, const char* value) {
+  //Create Null key for deleted elements
+  const int polynomial_const = 33;
+  //polynomial_const = 33 gives at most 6 collisions on a set of 50,000 English words
+  int hash = hash_code(key, polynomial_const);
+  int index = compress_function(hash, ht_const1, ht_const2, TABLESIZE);
+  int second_hash = second_function(hash, SECONDCONST);
+  
 }
--- src/hash_table.h
+++ src/hash_table.h
@@ -1,13 +1,19 @@
 #define TABLESIZE 59 //This number must be a prime
+#define SECONDCONST 53 //This must be smaller than TABLESIZE and is also a prime
 
 typedef struct {
   char* key;
   char* value;
-} pair;
+} str_pair;
 
 typedef struct {
   int count;
   int size;
-  pair** items;
-} table; 
+  str_pair** items;
+} hash_table; 
 
+void ht_insert(hash_table* ht, const char* key, const char* value);
+char* ht_search(hash_table* ht, const char* key);
+void ht_delete(hash_table* ht, const char* key);
+int ht_const1, ht_const2;
+void init_constant();
